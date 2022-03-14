1219d69fc54c76f0b67d6eb911843ceb58e2972c
marisasaunders <marisaksaunders@gmail.com>
2022-03-14 15:54:46 -0400

    started eyes on me

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  function handleClick() {
+    
+  }
+  return (
+    <>
+    <button onClick={handleClick}>Eyes on Me</button>
+    </>
+  )
+}
+
+export default EyesOnMe;
